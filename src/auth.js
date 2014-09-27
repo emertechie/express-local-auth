3b98ddc6d86890e0f0515adb3ef65519b584d48b
Andrew Smith <emertechie@gmail.com>
2014-09-27 16:40:18 +0700

    Bug fix: use utils.handleError for unauthenticated scenarios (fixes issue with attempted redirect from a JSON API mode usage)

--- src/auth.js
+++ src/auth.js
@@ -249,7 +249,7 @@ module.exports = function(sharedServices, options) {
                                 next(err);
                             });
                         } else {
-                            res.redirect(options.loginPath);
+                            return utils.handleError('Unauthenticated', { errorRedirect: options.loginPath }, 401)(req, res, next);
                         }
                     });
                 }
